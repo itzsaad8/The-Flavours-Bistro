12f10da3184b1673bb90075ee682201b38cb75b5
Waqas Mehmood <waqmeh456@gmail.com>
2023-11-10 17:15:17 +0500

    footer completed

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { BiLogoFacebook } from "react-icons/bi";
+import { BiLogoInstagram } from "react-icons/bi";
+import { BsShop } from "react-icons/bs";
+
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="container">
+                    <div className="row py-5">
+                        <div className="col-lg-3 col-12">
+                            <h5 className='footer_heading'>CONTACT US</h5>
+                            <p className='mt-3'>07975 553654</p>
+                            <p>07928 486245</p>
+                            <p>bennyg1231@outlook.com</p>
+                        </div>
+                        <div className="col-lg-3 col-12">
+                            <h5 className='footer_heading'>FIND US</h5>
+                            <p className='mt-3'>Unit 19 Withybush Trading Est Withybush Rd, Haverfordwest, SA62 4BS</p>
+
+                            <p className='footer_icons'><span className='footer_icon'><BiLogoFacebook /></span> <span className='footer_icon'><BiLogoInstagram /></span> <span className='footer_icon'><BsShop /></span> </p>
+                        </div>
+                        <div className="col-lg-3 col-12">
+                            <h5 className='footer_heading'>BUSINESS HOURS</h5>
+                            <p className='d-flex justify-content-between mt-3'><span>Monday</span><span>Closed</span></p>
+                            <p className='d-flex justify-content-between'><span>Tue - Fri</span><span>12:00 pm - 3:00 pm</span></p>
+                            <p className='d-flex justify-content-end'><span>5:00 pm - 8:00 pm</span></p>
+                            <p className='d-flex justify-content-between'><span>Saturday</span><span>5:00 pm - 8:00 pm</span></p>
+                            <p className='d-flex justify-content-between'><span>Sunday</span><span>Closed</span></p>
+                        </div>
+                        <div className="col-lg-3 col-12">
+                            <h5 className='footer_heading'>WE ACCEPT</h5>
+                            <div className="col-lg-12 col-3 payment_line mt-3">
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                            </div>
+                            <div className="col-lg-12 col-3 payment_line2">
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                                <div className='payments'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+            <div className="container-fluid bg-black py-1">
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-center">
+                        <p className='text-white d-flex gap-2'><span>Terms of Use</span>  |  <span>Privacy & Cookie Policy</span>  |  <span>Trading Terms</span>  | <span>Powered by Yell Business</span></p>
+                    </div>
+                    <div className="col-12 mt-0">
+                        <p className='text-white text-center'>&copy; 2023. The content on this website is owned by us and our licensors. Do not copy any content (including images) without our consent.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
