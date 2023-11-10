fec3ff190c6f536a6bcb677940860d7f6ee9a03f
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2023-11-10 16:40:38 +0500

    some changes

--- client/src/Pages/Gallery Page/SubComponents/TopSection.jsx
+++ client/src/Pages/Gallery Page/SubComponents/TopSection.jsx
@@ -7,10 +7,10 @@ const TopSection = ({title,description}) => {
                 <div className='container '>
                     <div className="row">
                         <div className="col">
-                            <div className='text-white text-center my-5  custom-center'>
-                                <h1>{title}</h1>
-                                <p>{description}</p>
-                            </div>
+                            <div className='text-center d-flex flex-column justify-content-center custom-height my-5 text-white'>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
                         </div>
                     </div>
                 </div>
