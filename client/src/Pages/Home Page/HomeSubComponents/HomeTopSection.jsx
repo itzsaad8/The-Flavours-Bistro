5f5d8bef471bcf657485777a8893d7efce243268
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2023-11-10 21:35:38 +0500

    little bit home page created

---
+++ client/src/Pages/Home Page/HomeSubComponents/HomeTopSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeTopSection = ({content}) => {
+  return (
+    <>
+      <div className="container-fluid d-flex justity-content-center align-items-center text-white text-center " id='home-bg'>
+        <h1>{content}</h1>
+
+</div>
+    </>
+  )
+}
+
+export default HomeTopSection
